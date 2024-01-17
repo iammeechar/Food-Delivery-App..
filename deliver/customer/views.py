a2bde1da6c3887c31be2e5c951e30b4fc36fdc75
iammeechar <aldomicha@gmail.com>
2024-01-17 20:05:17 +0300

    set up viwes in the customer section to get requests from index, about page and order page. Also created the base, index and about html folders

--- deliver/customer/views.py
+++ deliver/customer/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+#create an index view to handle any get requests.
+class Index(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'customer/index.html')#render html template
+    
+class About(View):
+     def get(self, request, *args, **kwargs):
+        return render(request, 'customer/about.html')#render in the about page
+
+class Order(View):
+    def get(self, request, *args, **kwargs):
+        
+        #get each item from each category
+        appetizers = MenuItems.objects.filter(category_name_contains='Appetizer')
+        entres = MenuItems.objects.filter(category_name_contains='Entre')
+        desserts = MenuItems.objects.filter(category_name_contains='Desserts')
+        drinks = MenuItems.objects.filter(category_name_contains='Drinks')
+        #pass into context
+        context = {
+            'appetizers': appetizers,
+            'entres': entres,
+            'desserts': desserts,
+            'drinks': drinks,
+
+        }
+
+        #render the template
