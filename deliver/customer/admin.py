e1455aebb65ae50c3124dea84248520daf9c1c82
iammeechar <aldomicha@gmail.com>
2024-01-21 18:55:19 +0300

    create admin registers for the menu, categories and order

--- deliver/customer/admin.py
+++ deliver/customer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import MenuItems, Category, OrderModel
 
-# Register your models here.
+#added the menu items, categories and order model to the admin page.
+admin.site.register(MenuItems)
+admin.site.register(Category)
+admin.site.register(OrderModel)
