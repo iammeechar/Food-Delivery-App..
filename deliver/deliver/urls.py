00551bde9a1e19fb60530f6d71b20707a4c22516
iammeechar <aldomicha@gmail.com>
2024-01-21 19:33:46 +0300

    add order path to the urls

--- deliver/deliver/urls.py
+++ deliver/deliver/urls.py
@@ -17,11 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static 
-from customer.views import Index, About #import links to the index and about pages.
+from customer.views import Index, About, Order #import links to the index and about pages.
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     #create paths to access the index and about pages.
     path('', Index.as_view(), name='index'),
     path('about/', About.as_view(), name='about'),
+    path('order/', Order.as_view(), name='order')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
